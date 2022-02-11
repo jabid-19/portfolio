0a73ac918283963b08fbc778205eb30005e6fd40
jabid19 <jabid19191@gmail.com>
2022-02-11 19:11:23 +0600

    imagepath

--- helper/imageLoader.js
+++ helper/imageLoader.js
@@ -1,5 +1,5 @@
 const imageLoader = ({ src, width, quality }) => {
-  return `https://portfolio-edxf2fg38-jabid-19.vercel.app/${src}?w=${width}&q=${
+  return `https://portfolio-jabid-19.vercel.app/${src}?w=${width}&q=${
     quality || 80
   }}`;
   // return `http://localhost:3006/${src}?w=${width}&q=${quality || 100}}`;
